115ea39e3dd71c2a710745b79551f94075129f76
Takaaki <tkakmtst@gmail.com>
2025-01-13 00:55:50 +0900

    Refactor vpc module

--- aws/envs/stg/terraform.tfvars
+++ aws/envs/stg/terraform.tfvars
@@ -1,2 +1,5 @@
-cidr_block  = "10.0.0.0/16"
-environment = "stg"
+cidr_block                 = "10.0.0.0/16"
+environment                = "stg"
+public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+private_subnet_cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+availability_zones         = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
--- aws/modules/vpc/variables.tf
+++ aws/modules/vpc/variables.tf
@@ -11,17 +11,14 @@ variable "environment" {
 variable "public_subnet_cidr_blocks" {
   description = "CIDR blocks for the public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidr_blocks" {
   description = "CIDR blocks for the private subnets"
   type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
 }
